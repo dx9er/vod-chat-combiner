38b58927ff8fbed47eaae600b43eb6e244710682
dx9er <dx9@disroot.org>
2023-10-18 03:11:32 +0300

    Fix ffmpeg location in modes

--- modes/masked.sh
+++ modes/masked.sh
@@ -6,7 +6,7 @@ $(twitch_dl "chatrender") -i "${VOD_ID}/chat.json" -o "${VOD_ID}/chat.mp4" \
     --emoji-vendor "$([[ -n $EMOJI_VENDOR ]] && echo -n "--emoji-vendor ${EMOJI_VENDOR}" || echo -n "notocolor")" \
     $([[ -n $CHAT_FONT ]] && echo -n "--font "${CHAT_FONT}"") \
     --outline true --generate-mask --background-color '#00000000' \
-    --ffmpeg-path "$([[ -n $FFMPEG_PATH ]] && echo -n "${FFMPEG_PATH}/ffmpeg" || echo -n "${SCRIPT_DIR}/bin/ffmpeg")" && \
+    --ffmpeg-path "$([[ -n $FFMPEG_PATH ]] && echo -n "${FFMPEG_PATH}/ffmpeg" || echo -n "${COMBINER_DIR}/bin/ffmpeg")" && \
 # Render with masked chat
 # https://github.com/lay295/TwitchDownloader/issues/79#issuecomment-699648513
 ffmpeg -y  -i "${VOD_ID}/vod.mp4" -i "${VOD_ID}/chat.mp4" -i "${VOD_ID}/chat_mask.mp4" \
--- modes/sidebyside.sh
+++ modes/sidebyside.sh
@@ -5,7 +5,7 @@ $(twitch_dl "chatrender") -i "${VOD_ID}/chat.json" -o "${VOD_ID}/chat.mp4" \
     --update-rate 0 --dispersion true \
     --emoji-vendor "$([[ -n $EMOJI_VENDOR ]] && echo -n "--emoji-vendor ${EMOJI_VENDOR}" || echo -n "notocolor")" \
     $([[ -n $CHAT_FONT ]] && echo -n "--font "${CHAT_FONT}"") \
-    --ffmpeg-path "$([[ -n $FFMPEG_PATH ]] && echo -n "${FFMPEG_PATH}/ffmpeg" || echo -n "${SCRIPT_DIR}/bin/ffmpeg")" && \
+    --ffmpeg-path "$([[ -n $FFMPEG_PATH ]] && echo -n "${FFMPEG_PATH}/ffmpeg" || echo -n "${COMBINER_DIR}/bin/ffmpeg")" && \
 # Side by side render
 ffmpeg -hide_banner -y -i "${VOD_ID}/vod.mp4" -i "${VOD_ID}/chat.mp4" -filter_complex hstack \
     "$([[ -n $OUTPUT ]] && echo -n $OUTPUT || echo -n "$VOD_ID.mp4")"
