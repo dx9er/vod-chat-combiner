c32312441b681eeebfd35d5170685aa8b5655953
dx9er <dx9@disroot.org>
2023-10-19 21:46:59 +0300

    Add README

--- modes/sidebyside.sh
+++ modes/sidebyside.sh
@@ -7,5 +7,6 @@ $(twitch_dl "chatrender") -i "${VOD_ID}/chat.json" -o "${VOD_ID}/chat.mp4" \
     $([[ -n $CHAT_FONT ]] && echo -n "--font "${CHAT_FONT}"") \
     --ffmpeg-path "$([[ -n $FFMPEG_PATH ]] && echo -n "${FFMPEG_PATH}/ffmpeg" || echo -n "${COMBINER_DIR}/bin/ffmpeg")" && \
 # Side by side render
+# TODO: Only supports 1080p, use ffprobe(?) to determine height
 ffmpeg -hide_banner -y -i "${VOD_ID}/vod.mp4" -i "${VOD_ID}/chat.mp4" -filter_complex hstack \
     "$([[ -n $OUTPUT ]] && echo -n $OUTPUT || echo -n "$VOD_ID.mp4")"
